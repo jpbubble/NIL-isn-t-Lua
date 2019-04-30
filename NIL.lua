e5741aac051c259a862a92c0c41caee5894661d6
Jeroen P. Broks <facebook@tbbs.nl>
2019-04-30 09:42:36 +0200

    Prep for translation

--- NIL.lua
+++ NIL.lua
@@ -6,21 +6,83 @@
 
 ]]
 
-
+-- Variables
 local macros = {}
 local vars = {}
 local luakeywords = {}
-local NIL = {}
+local mNIL = {}
 
-function NIL.Load(script,chunk)
+-- locals are faster than gloabls
+local io=io
+local assert=assert
+local load=load
+local string=string
+local ipairs=ipairs
+local pairs=pairs
+
+-- A few functions I need to get NIL to work anyway!
+local function split(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; local i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
 end
 
-NIL.LoadString = NIL.Load
+local substr = string.sub
+local function left(s,l)
+return substr(s,1,l)
+end local Left=left
 
-function NIL.LoadFile(file,chunk)
+local function right(s,l)
+local ln = l or 1
+local st = s or "nostring"
+return substr(st,-ln,-1)
+end local Right=right
+
+local function mid(s,o,l)
+  local ln=l or 1
+  local of=o or 1
+  local st=s or ""
+  return substr(st,of,(of+ln)-1)
+end local Mid=mid
+
+local function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+local function itpairs(mytab)
+  local i = 0
+  return function()
+      i = i + 1
+      if mytab[i] then return i,trim(mytab[i]) end
+      return nil,nil
+  end
+end
+
+
+-- Translator itself
+function mNIL.Translate(script,chunk)
+    local lines = split(script,"\n")
+    local lmacro = {}
+    for linenumber,line in itpairs(lines) do
+    end
+end
+
+function mNIL.Load(script,chunk)
+    return load(mNIL.Translate(script,chunk))
+end
+
+mNIL.LoadString = mNIL.Load
+
+function mNIL.LoadFile(file,chunk)
 	local f = assert(io.open(file, "rb"))
 	local content = f:read("*all")
 	f:close()
-	return NIL.Load(content,chunk or file)
+	return mNIL.Load(content,chunk or file)
 end
 
