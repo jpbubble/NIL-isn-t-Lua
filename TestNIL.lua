a9a2092cf22420fce783873288cad29facff33ef
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-01 13:42:39 +0200

    Test Script

---
+++ TestNIL.lua
@@ -0,0 +1,17 @@
+--[[
+
+   All this program does is testing NIL.
+   You won't need this file in your projects, it's only for testing purposes!
+   
+]]
+
+local NIL=require "NIL"
+
+local NILCode = [[
+    -- Testing!
+    #macro HW "Hello World!"
+    print(HW)
+]]
+local NILTrans = NIL.Translate(NILCode,"NILCode")
+
+print("Original:\n"..NILCode,"\n\nTranslation:\n"..NILTrans)
