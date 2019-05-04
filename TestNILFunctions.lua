597d0f09d4ea57d7a19cf494275836bf07aa5c1f
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-04 22:38:21 +0200

    closed #6

--- TestNILFunctions.lua
+++ TestNILFunctions.lua
@@ -1,14 +1,23 @@
 --[[
+***********************************************************
+TestNILFunctions.lua
+This particular file has been released in the public domain
+and is therefore free of any restriction. You are allowed
+to credit me as the original author, but this is not
+required.
+This file was setup/modified in:
 
-    test functions
-    Since functions are BY FAR more complex than
-    the stuff I've set up so far, I decided to put in the own department for functions.
-    
-    This is also because I want NIL to support something Lua does not support. 
-    Class types. Having said that I guess it's only fitting I want to do this right
-    from the start.and
-    
+If the law of your country does not support the concept
+of a product being released in the public domain, while
+the original author is still alive, or if his death was
+not longer than 70 years ago, you can deem this file
+"(c) Jeroen Broks - licensed under the CC0 License",
+with basically comes down to the same lack of
+restriction the public domain offers. (YAY!)
+***********************************************************
+Version 19.05.04
 ]]
+
 
 
 
