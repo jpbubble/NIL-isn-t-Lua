94002d187a50074aed64aed3d2858e544c32eb97
Jeroen P. Broks <facebook@tbbs.nl>
2019-05-07 08:40:25 +0200

    Testing assigning existing function to a delegate!
    The real challenge awaits though... A DIRECT declaration!

---
+++ TestNILDelegate.lua
@@ -0,0 +1,20 @@
+
+local NILCode = [[
+       void Hello()
+          print("Hello World")
+       end
+       
+       delegate Hi
+       Hi = Hello
+       Hi()
+]]
+
+local NIL=require "NIL"
+local NILTrans = NIL.Translate(NILCode,"NILCode")
+
+print("Original:\n"..NILCode,"\n\nTranslation:\n"..NILTrans)
+
+print("Executing transation!")
+local l = loadstring(NILTrans,"Translation")
+l()
+
