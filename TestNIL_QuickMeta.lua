09e4d21f514f43c5ca83116cfcc1aded3e226e9b
Jeroen P. Broks <facebook@tbbs.nl>
2019-08-26 11:15:10 +0200

    vUpdate

--- TestNIL_QuickMeta.lua
+++ TestNIL_QuickMeta.lua
@@ -1,3 +1,23 @@
+--[[
+***********************************************************
+TestNIL_QuickMeta.lua
+This particular file has been released in the public domain
+and is therefore free of any restriction. You are allowed
+to credit me as the original author, but this is not
+required.
+This file was setup/modified in:
+
+If the law of your country does not support the concept
+of a product being released in the public domain, while
+the original author is still alive, or if his death was
+not longer than 70 years ago, you can deem this file
+"(c) Jeroen Broks - licensed under the CC0 License",
+with basically comes down to the same lack of
+restriction the public domain offers. (YAY!)
+***********************************************************
+Version 19.08.26
+]]
+
 NIL = require "NIL"
 scriptfile = "TestNIL_QuickMeta.nil"
 function gs(file)
@@ -11,3 +31,4 @@ t = NIL.Translate(script)
 print(t)
 compiled = assert((loadstring or load)(t,"TRANSLATION"))
 compiled()
+
--- TestNIL_With.lua
+++ TestNIL_With.lua
@@ -1,3 +1,23 @@
+--[[
+***********************************************************
+TestNIL_With.lua
+This particular file has been released in the public domain
+and is therefore free of any restriction. You are allowed
+to credit me as the original author, but this is not
+required.
+This file was setup/modified in:
+
+If the law of your country does not support the concept
+of a product being released in the public domain, while
+the original author is still alive, or if his death was
+not longer than 70 years ago, you can deem this file
+"(c) Jeroen Broks - licensed under the CC0 License",
+with basically comes down to the same lack of
+restriction the public domain offers. (YAY!)
+***********************************************************
+Version 19.08.26
+]]
+
 NIL = require "NIL"
 
 function gs(file)
@@ -16,4 +36,5 @@ print("*** ORIGINAL ***\n\n"..script_nil.."\n\n\n*** TRANSLATION ***\n\n"..scrip
 script_compiled = loadstring(script_lua)
 
 script_compiled()
+
 
